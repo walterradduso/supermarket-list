50e360258762cd38a95f4436a9d049eea6ae5cd7
Walter Radduso <w.radduso@gmail.com>
2022-01-23 15:23:40 -0300

    [FTR] - Test on the challenge: - Added some tests.

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,24 +1,55 @@
 import React from "react";
 import {render, screen, waitFor} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import App from "./App";
 
-test("renders 'loading supermarket list' text", () => {
-  render(<App />);
+describe("App Tests", () => {
+  test("render snapshot", async () => {
+    const appRendered = render(<App />);
 
-  const introElement = screen.getByText(/Loading supermarket list.../i);
+    expect(appRendered).toMatchSnapshot();
+  });
 
-  expect(introElement).toBeInTheDocument();
-});
+  test("renders 'loading supermarket list' text", () => {
+    render(<App />);
 
-test("renders 'Add Item' text", async () => {
-  render(<App />);
+    const introElement = screen.getByText("Loading supermarket list items...");
 
-  await waitFor(() => expect(screen.getByText(/Add Item/i)).toBeInTheDocument());
-});
+    expect(introElement).toBeInTheDocument();
+  });
 
-test("render snapshot", async () => {
-  const appRendered = render(<App />);
+  test("renders 'Add Item' text", async () => {
+    render(<App />);
 
-  expect(appRendered).toMatchSnapshot();
+    await waitFor(() => expect(screen.getByText("Add Item")).toBeInTheDocument());
+  });
+
+  test("renders open and close modal", async () => {
+    render(<App />);
+
+    await waitFor(() => {
+      userEvent.click(screen.getByText("Add Item"));
+      expect(screen.getByText("Cancel")).toBeInTheDocument();
+
+      userEvent.click(screen.getByText("Cancel"));
+      expect(screen.queryByText("Cancel")).not.toBeInTheDocument();
+    });
+  });
+
+  test("renders close modal clicking outside", async () => {
+    render(<App />);
+
+    await waitFor(() => {
+      userEvent.click(screen.getByText("Add Item"));
+
+      const outsideModal = document.querySelector("b");
+
+      if (outsideModal) {
+        outsideModal.click();
+      }
+
+      expect(screen.queryByText("Cancel")).not.toBeInTheDocument();
+    });
+  });
 });
